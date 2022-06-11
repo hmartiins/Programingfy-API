ac30a967a1d38c8493297194aa34b1e498da578a
hmartiins <hmartins224@gmail.com>
2022-06-11 19:46:00 -0300

    Create Spring Boot project

---
+++ src/main/java/com/example/programingfyapi/ProgramingfyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.programingfyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramingfyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramingfyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/programingfyapi/ProgramingfyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programingfyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramingfyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
